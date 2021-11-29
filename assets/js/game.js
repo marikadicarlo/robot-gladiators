f3cb4cbf786dd892bc67f749054ed0e342af3a8a
marikadicarlo <mdicarlo19@yahoo.com>
2021-11-29 11:46:08 -0500

    player name prompt debug

--- assets/js/game.js
+++ assets/js/game.js
@@ -170,8 +170,21 @@ var shop = function() {
 /* GAME INFORMATION / VARIABLES */
 
 // player information
+var getPlayerName = function() {
+  var name = "";
+
+// ***************************************
+while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+// ***************************************
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
